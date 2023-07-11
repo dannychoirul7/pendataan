5a23bdd1e0a90cb99095b99cb7bda0b3f3bc79f5
dannychoirul7 <61981632+dannychoirul7@users.noreply.github.com>
2023-07-11 21:05:37 +0700

    Add files via upload
    re upload full tampil jenis barang

---
+++ jenis/index.php
@@ -0,0 +1,49 @@
+<?php
+require '../header.php';
+require "fungsi.php";
+$jenis_barang = query("SELECT * FROM jenis_barang ORDER BY id_jenis_barang Asc");
+?>
+
+<body>
+  <?php
+  require '../side.php';
+  ?>
+  <div class="p-4" id="main-content">
+    <button class="btn btn-primary" id="button-toggle">
+      Toggle Menu
+    </button>
+    <div class="card mt-5">
+      <div class="card-body">
+        <h1 style="text-align:center">Jenis Barang </h1>
+        <table class="table table-striped">
+          <thead>
+            <tr style="text-align:center">
+              <th style="width: 15%">No</th>
+              <th>Jenis Barang</th>
+              <th style="width: 15%">
+                <a class="btn btn-sm btn-primary" href="tambah.php" role="button">tambah data</a>
+              </th>
+            </tr>
+          <tbody>
+            <?php $no = 1; ?>
+            <?php foreach ($jenis_barang as $row) : ?>
+              <tr>
+                <td width="5%" style="text-align: center;" scope="row"><?= $no; ?></td>
+                <td style="text-align: center;"><?= $row["nama_jenis_barang"]; ?></td>
+                <td style="text-align: center">
+                  <a class="btn btn-sm btn-warning" href="#" role="button">ubah</a>
+                  <a class="btn btn-sm btn-danger" href="#" onclick="return confirm('yakin?');" role="button">hapus</a>
+                </td>
+              </tr>
+            <?php $no++;
+            endforeach; ?>
+          </tbody>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+<?php require '../footer.php'; ?>
+
+</html>
